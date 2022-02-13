42c42a3c87f63528ba2972024e53b6a0a9e6f4e5
Felipe Dioguardi <felipedioguardi@gmail.com>
2022-02-13 15:11:44 -0300

    refactor: add type hints to credentials

--- spunify/credentials.py
+++ spunify/credentials.py
@@ -1,8 +1,8 @@
 """ Constants for the credentials for the Authorization flow. """
 
-CLIENT_ID = "7679e0ed5e7248a28981b3a2eb3803cb"
-REDIRECT_URI = "http://127.0.0.1:9090"
-SCOPE = [
+CLIENT_ID: str = "7679e0ed5e7248a28981b3a2eb3803cb"
+REDIRECT_URI: str = "http://127.0.0.1:9090"
+SCOPE: list[str] | str = [
     "playlist-modify-private",
     "playlist-read-collaborative",
     "playlist-read-private",
